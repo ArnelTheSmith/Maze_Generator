eb1e5bdc6502215a8c405ba44345a5029c6bf8cd
Arnel <gabrielmartinez058@gmail.com>
2023-06-12 08:15:02 -0500

    Draw cell move

---
+++ cell.py
@@ -0,0 +1,52 @@
+from graphics import Point, Line
+class Cell:
+        def __init__(self,win,l_wall=True,r_wall=True,t_wall=True,b_wall=True):
+            self.size = 80
+            self.l_wall = l_wall
+            self.r_wall = r_wall
+            self.t_wall = t_wall
+            self.b_wall = b_wall
+            self._x = None
+            self._x2 = None
+            self._y= None
+            self._y2 = None
+            self._win= win
+
+        def draw(self,x,y):
+            self._x = x
+            self._x2 = x + self.size
+            self._y= y
+            self._y2 = y + self.size
+
+            #Draw existing walls
+            if self.l_wall:
+               wall=Line(Point(x,y),Point(x,y+self.size))
+               self._win.draw_line(wall)
+
+            if self.r_wall:
+               wall=Line(Point(x+self.size,y),Point(x+self.size,y+self.size))
+               self._win.draw_line(wall)
+
+            if self.t_wall:
+               wall=Line(Point(x,y),Point(x+self.size,y))
+               self._win.draw_line(wall)
+
+            if self.b_wall:
+               wall=Line(Point(x,y+self.size),Point(x+self.size,y+self.size))
+               self._win.draw_line(wall)
+        
+        def draw_move(self,to_cell):
+            mid_p = Point(((self._x * 2) + self.size)/2, ((self._y * 2) + self.size)/2)
+            if to_cell == "l":
+                line = Line(mid_p, mid_p.translateX(-self.size))
+                self._win.draw_line(line)
+            if to_cell == "r":
+                line = Line(mid_p,mid_p.translateX(self.size))
+                self._win.draw_line(line)
+            if to_cell == "t":
+                line = Line(mid_p,mid_p.translateY(-self.size))
+                self._win.draw_line(line)
+            if to_cell == "b":
+                line = Line(mid_p,mid_p.translateY(self.size))
+                self._win.draw_line(line)
+            
--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-from graphics import Window, Point, Line, Cell
+from graphics import Window, Point, Line
+from cell import Cell
 
-cell_list= []
-
-pos={"x1":50,"y1":50,"x2":100,"y2":100}
-
-for n in range(6):
-    n = Cell(pos["x1"]+(n*100), pos["y1"])
-    cell_list.append(n)
-    
 win = Window(800, 600)
-cell = Cell(100,100)
+cell= Cell(win)
+cell.draw(100, 100)
+cell.draw_move("r")
 
-
-for n in range(6):
-    win.draw_cell(cell_list[n])
 
 win.wait_for_close()
